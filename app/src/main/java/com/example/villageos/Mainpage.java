8518ea903d4d67d8ddd2d822bb70da6a15c17f49
PC <ranveerdhumal00@gmail.com>
2026-06-06 15:32:55 +0530

    Initial VillageOS Project

--- app/src/main/java/com/example/villageos/Mainpage.java
+++ app/src/main/java/com/example/villageos/Mainpage.java
@@ -1,6 +1,9 @@
 package com.example.villageos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +16,16 @@ public class Mainpage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mainpage2);
+        setContentView(R.layout.activity_mainpage);
 
+        Button btn=findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Mainpage.this, loginpage.class);
+                startActivity(i);
+            }
+        });
     }
 }
