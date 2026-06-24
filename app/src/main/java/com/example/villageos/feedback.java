8b261bdf75a042f00c6ed7d915b00cfab50cfd42
DELL <ranveerdhumal00@gmail.com>
2026-06-24 19:50:50 +0530

    all clear

--- app/src/main/java/com/example/villageos/feedback.java
+++ app/src/main/java/com/example/villageos/feedback.java
@@ -1,6 +1,11 @@
 package com.example.villageos;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +13,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class feedback extends AppCompatActivity {
+    DatabaseReference db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_feedback);
 
+        setContentView(R.layout.activity_feedback);
+        RatingBar r1=findViewById(R.id.r1);
+        EditText e2=findViewById(R.id.e2);
+        Button btn=findViewById(R.id.btn);
+        db= FirebaseDatabase.getInstance().getReference("Feedback");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String RATING=""+r1.getRating();
+                String SUGGESION=e2.getText().toString();
+                feed f=new feed(RATING,SUGGESION);
+                String uid=db.push().getKey();
+                db.child(uid).setValue(f).addOnSuccessListener(unused -> Toast.makeText(feedback.this, "Store", Toast.LENGTH_LONG).show()).addOnFailureListener(e -> Toast.makeText(feedback.this, "Fail", Toast.LENGTH_LONG).show());
+            }
+        });
     }
 }
+
+
+
